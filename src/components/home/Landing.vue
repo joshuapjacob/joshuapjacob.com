480c3b910cb153eb7b72e678bf4bb416c8204aa5
Joshua Paul Jacob <contact@joshuapjacob.com>
2020-08-26 15:52:38 +0200

    Update threejs in Landing.vue

--- src/components/home/Landing.vue
+++ src/components/home/Landing.vue
@@ -32,35 +32,43 @@ export default {
     return {
       composer: null,
       customPass: null,
-      uMouse: null,
+      camera: null,
+      renderer: null,
+      mouse: null,
+      prevMouse: null,
+      followMouse: null,
+      targetSpeed: null,
+      speed: null,
+      time: null,
     };
   },
   methods: {
     init: function () {
       const scene = new Scene();
-      const camera = new PerspectiveCamera(
+      this.camera = new PerspectiveCamera(
         75,
         window.innerWidth / window.innerHeight,
         0.01,
         10
       );
-      camera.position.z = 0.5;
-      const renderer = new WebGLRenderer({ antialias: true });
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      renderer.outputEncoding = sRGBEncoding;
+      this.camera.position.z = 0.5;
+      this.renderer = new WebGLRenderer({ antialias: true, alpha: true });
+      this.renderer.setPixelRatio(1);
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+      this.renderer.outputEncoding = sRGBEncoding;
       const container = document.getElementById("canvas_container");
-      container.appendChild(renderer.domElement);
+      container.appendChild(this.renderer.domElement);
 
       const texture = new TextureLoader().load(require("@/assets/bg.jpg"));
       const mesh = new Mesh(
-        new PlaneBufferGeometry(1, 0.5625),
+        new PlaneBufferGeometry(2 * 0.6, 1.125 * 0.6),
         new MeshBasicMaterial({ map: texture })
       );
       scene.add(mesh);
 
       // Post-Processing
-      this.composer = new EffectComposer(renderer);
-      const renderPass = new RenderPass(scene, camera);
+      this.composer = new EffectComposer(this.renderer);
+      const renderPass = new RenderPass(scene, this.camera);
       this.composer.addPass(renderPass);
 
       const myEffect = {
@@ -71,45 +79,112 @@ export default {
           },
           uMouse: { value: new Vector2(-10, -10) },
           uVelo: { value: 0 },
+          time: { value: 0 },
         },
-        vertexShader: `varying vec2 vUv;void main() {vUv = uv;gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0 );}`,
-        fragmentShader: `uniform float time;
-        uniform sampler2D tDiffuse;
-        uniform vec2 resolution;
-        varying vec2 vUv;
-        uniform vec2 uMouse;
-        float circle(vec2 uv, vec2 disc_center, float disc_radius, float border_size) {
-          uv -= disc_center;
-          uv*=resolution;
-          float dist = sqrt(dot(uv, uv));
-          return smoothstep(disc_radius+border_size, disc_radius-border_size, dist);
-        }
-        void main()  {
+        vertexShader: `
+          uniform float time;
+          uniform float progress;
+          uniform vec2 resolution;
+          varying vec2 vUv;
+          uniform sampler2D texture1;
+
+          const float pi = 3.1415925;
+
+          void main() {
+            vUv = uv;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0 );
+          }
+        `,
+        fragmentShader: `
+          uniform float time;
+          uniform float progress;
+          uniform sampler2D tDiffuse;
+          uniform vec2 resolution;
+          varying vec2 vUv;
+          uniform vec2 uMouse;
+          uniform float uVelo;
+          uniform int uType;
+
+
+          float circle(vec2 uv, vec2 disc_center, float disc_radius, float border_size) {
+            uv -= disc_center;
+            uv *= resolution*0.5;
+            float dist = sqrt(dot(uv, uv));
+            return smoothstep(disc_radius+border_size, disc_radius-border_size, dist);
+          }
+
+
+          void main()	{
             vec2 newUV = vUv;
-            float c = circle(vUv, uMouse, 0.0, 0.2);
-            float r = texture2D(tDiffuse, newUV.xy += c * (0.1 * .5)).x;
-            float g = texture2D(tDiffuse, newUV.xy += c * (0.1 * .525)).y;
-            float b = texture2D(tDiffuse, newUV.xy += c * (0.1 * .55)).z;
-            vec4 color = vec4(r, g, b, 1.);
+            vec4 color = vec4(1.,0.,0.,1.);
+
+              float c = circle(newUV, uMouse, 0.0, 0.2);
+              float r = texture2D(tDiffuse, newUV.xy += c * (uVelo * .5)).x;
+              float g = texture2D(tDiffuse, newUV.xy += c * (uVelo * .525)).y;
+              float b = texture2D(tDiffuse, newUV.xy += c * (uVelo * .55)).z;
+              color = vec4(r, g, b, 1.);
+
             gl_FragColor = color;
-        }`,
+          }
+        `,
       };
 
       this.customPass = new ShaderPass(myEffect);
       this.customPass.renderToScreen = true;
       this.composer.addPass(this.customPass);
 
-      this.uMouse = new Vector2(0, 0);
+      this.speed = 0;
+      this.time = 0;
+      this.targetSpeed = 0;
+
+      this.mouse = new Vector2(0, 0);
+      this.prevMouse = new Vector2(0, 0);
+      this.followMouse = new Vector2(0, 0);
       document.addEventListener("mousemove", this.handleMouse);
+
+      window.addEventListener("resize", this.handleResize);
+    },
+    updateSpeed: function () {
+      this.speed = Math.sqrt(
+        (this.prevMouse.x - this.mouse.x) ** 2 +
+          (this.prevMouse.y - this.mouse.y) ** 2
+      );
+
+      this.targetSpeed -= 0.1 * (this.targetSpeed - this.speed);
+      this.followMouse.x -= 0.1 * (this.followMouse.x - this.mouse.x);
+      this.followMouse.y -= 0.1 * (this.followMouse.y - this.mouse.y);
+
+      this.prevMouse.x = this.mouse.x;
+      this.prevMouse.y = this.mouse.y;
     },
     animate: function () {
-      this.customPass.uniforms.uMouse.value = this.uMouse;
+      this.time += 0.05;
+      this.updateSpeed();
+      this.customPass.uniforms.time.value = this.time;
+      this.customPass.uniforms.uMouse.value = this.followMouse;
+      this.customPass.uniforms.uVelo.value = Math.min(this.targetSpeed, 0.05);
+      this.targetSpeed *= 0.999;
       requestAnimationFrame(this.animate);
       this.composer.render();
     },
     handleMouse: function (e) {
-      this.uMouse.x = e.clientX / window.innerWidth;
-      this.uMouse.y = 1 - e.clientY / window.innerHeight;
+      this.mouse.x = e.clientX / window.innerWidth;
+      this.mouse.y = 1 - e.clientY / window.innerHeight;
+    },
+    handleResize: function () {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      this.renderer.setSize(width, height);
+      this.camera.aspect = width / height;
+
+      // this.camera.fov =
+      //   2 *
+      //   Math.atan(width / this.camera.aspect / (2 * this.cameraDistance)) *
+      //   (180 / Math.PI); // in degrees
+
+      this.customPass.uniforms.resolution.value.y = height / width;
+
+      this.camera.updateProjectionMatrix();
     },
   },
   mounted() {
@@ -118,6 +193,7 @@ export default {
   },
   beforeDestroy() {
     document.removeEventListener("mousemove", this.handleMouse);
+    window.removeEventListener("resize", this.handleResize);
   },
 };
 </script>
@@ -131,9 +207,9 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
-  -webkit-box-shadow: inset 0px -100px 100px 0px rgba(0, 0, 0, 1);
+  /* -webkit-box-shadow: inset 0px -100px 100px 0px rgba(0, 0, 0, 1);
   -moz-box-shadow: inset 0px -100px 100px 0px rgba(0, 0, 0, 1);
-  box-shadow: inset 0px -100px 100px 0px rgba(0, 0, 0, 1);
+  box-shadow: inset 0px -100px 100px 0px rgba(0, 0, 0, 1); */
   height: 100vh;
   width: 100vw;
 }
