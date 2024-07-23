706529caa1bd0a47754763f877b3e5172a104e14
Joshua P. Jacob <xozhioc@gmail.com>
2024-07-22 20:57:07 -0700

    Add Google Analytics

---
+++ src/lib/components/Analytics.svelte
@@ -0,0 +1,29 @@
+<!-- https://joyofcode.xyz/sveltekit-google-analytics -->
+
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	$: {
+		if (typeof gtag !== 'undefined') {
+			gtag('config', 'G-WTXGM4350N', {
+				page_title: document.title,
+				page_path: $page.url.pathname
+			});
+		}
+	}
+</script>
+
+<svelte:head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-WTXGM4350N">
+	</script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+
+		gtag('js', new Date());
+		gtag('config', 'G-WTXGM4350N');
+	</script>
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
 	import '../global.css';
+	import Analytics from '$lib/components/Analytics.svelte';
 </script>
 
+<Analytics />
 <slot />
